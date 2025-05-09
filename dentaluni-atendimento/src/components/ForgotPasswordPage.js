288a0e29264e7a1dbe8da5d35799d396899a95bf
GabrielGonzalesOdontic <dev.gabrielgonzales@gmail.com>
2025-05-09 10:47:03 -0300

    animações

--- dentaluni-atendimento/src/components/ForgotPasswordPage.js
+++ dentaluni-atendimento/src/components/ForgotPasswordPage.js
@@ -1,47 +1,81 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/ForgotPasswordPage.css';
-import LogoImage from '../img/logo.png'; 
-import HeroImage from '../img/hero-banner-bg.png'; 
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/ForgotPasswordPage.css";
+import Logo from "../img/logo.png";
+import { motion } from "framer-motion";
 
 const ForgotPasswordPage = () => {
-  const [email, setEmail] = useState('');
+  const [codAcesso, setCodAcesso] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Email para recuperação:', email);
-    alert(`Link de recuperação enviado para: ${email} (simulação)`);
+    console.log("Email para recuperação:", codAcesso);
+    alert(`Link de recuperação enviado para: ${codAcesso} (simulação)`);
   };
 
   return (
-    <div className="fp-page-layout">
-      <div className="hero-section">
-        <img src={HeroImage} alt="Hero background" className="hero-image" />
+    <div className="signin-page">
+      {" "}
+      <div className="signin-header">
+        {" "}
+        <div>
+          <motion.img
+            className="signin-animated-title logo"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2, ease: "easeOut" }}
+            src={Logo}
+            alt="Logo"
+          />
+        </div>
+        <motion.h1
+          className="signin-animated-title"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.3, ease: "easeOut" }}
+        >
+          Recuperar Senha
+        </motion.h1>
+        <motion.p
+          className="signin-animated-title"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.4, ease: "easeOut" }}
+        >
+          Insira seu código de acesso para enviarmos as instruções.
+        </motion.p>
       </div>
-      <div className="fp-container-centered"> 
-        <img src={LogoImage} alt="Logo da Empresa" className="fp-logo" />
-        <h2>Esqueceu sua senha?</h2>
-        <p className="fp-subtitle">
-          Não se preocupe! Insira seu e-mail abaixo e enviaremos um link para você cadastrar uma nova senha.
-        </p>
-        <form onSubmit={handleSubmit}>
-          <div className="fp-input-group">
+      <motion.div
+              className="signin-form-card"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.8, delay: 0.6, ease: "backOut" }}>
+        {" "}
+        <form onSubmit={handleSubmit} className="signin-form">
+          <div className="input-field-group">
+            <label htmlFor="email-forgot">Código de acesso</label>
             <input
-              type="email"
-              placeholder="Seu e-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              id="cod-forgot"
+              placeholder="Ex:90801"
+              value={codAcesso}
+              onChange={(e) => setCodAcesso(e.target.value)}
               required
             />
           </div>
-          <button type="submit" className="fp-button">
-            Enviar link de recuperação
+
+          <button type="submit" className="signin-button">
+            Enviar Link
           </button>
+
+          <div className="options-fp">
+            <Link to="/login" className="back-to-login-link">
+              Voltar para o Login
+            </Link>
+          </div>
         </form>
-        <div className="fp-back-to-login">
-          <Link to="/login">Voltar para o Login</Link>
-        </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
--- dentaluni-atendimento/src/components/LoginPage.js
+++ dentaluni-atendimento/src/components/LoginPage.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../styles/LoginPage.css"; 
-import { FaEye, FaEyeSlash } from "react-icons/fa"; 
-import nomeDoSeuLogo from '../img/logo.png'
+import "../styles/LoginPage.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Logo from "../img/logo.png";
+import { motion } from "framer-motion";
 
 const SigninPage = () => {
-  const [codigo, setCodigo] = useState(""); 
+  const [codigo, setCodigo] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -17,7 +18,7 @@ const SigninPage = () => {
     console.log("Senha:", password);
     console.log("Lembrar:", rememberMe);
     alert("Login simulado com sucesso!");
-    navigate("/menu"); 
+    navigate("/menu");
   };
 
   const togglePasswordVisibility = () => {
@@ -28,20 +29,46 @@ const SigninPage = () => {
     <div className="signin-page">
       <div className="signin-header">
         <div>
-          <img className="logo" src={nomeDoSeuLogo} alt="Logo"/>
+          <motion.img
+            className="signin-animated-title logo"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2, ease: "easeOut" }}
+            src={Logo}
+            alt="Logo"
+          />
         </div>
-        <h1>Bem-vindo de volta!</h1>
-        <p>Utilize seu usuário e senha do SIO para acessar seu perfil:</p>
+        <motion.h1
+          className="signin-animated-title"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.3, ease: "easeOut" }}
+        >
+          Bem-vindo de volta!
+        </motion.h1>
+        <motion.p
+          className="signin-animated-title"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.4, ease: "easeOut" }}
+        >
+          Utilize seu código e senha para continuar.
+        </motion.p>
       </div>
 
-      <div className="signin-form-card">
+      <motion.div
+        className="signin-form-card"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8, delay: 0.6, ease: "backOut" }}
+      >
         <form onSubmit={handleSubmit} className="signin-form">
           <div className="input-field-group">
             <label htmlFor="email">Código de acesso</label>
             <input
               type="codigo"
               id="codigo"
-              placeholder="Ex: 90801" 
+              placeholder="Ex: 90801"
               value={codigo}
               onChange={(e) => setCodigo(e.target.value)}
               required
@@ -52,7 +79,7 @@ const SigninPage = () => {
             <label htmlFor="password">Senha</label>
             <div className="password-input-wrapper">
               <input
-                type={passwordVisible ? "text" : "password"} 
+                type={passwordVisible ? "text" : "password"}
                 id="password"
                 placeholder="********"
                 value={password}
@@ -90,7 +117,7 @@ const SigninPage = () => {
             Entrar
           </button>
         </form>
-      </div>
+      </motion.div>
     </div>
   );
 };
